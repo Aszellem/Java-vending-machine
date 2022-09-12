faf95ec8c8948bb921fe4ed3379de1f90be259b1
aszellem <aszellemek@gmail.com>
2022-09-12 13:31:04 +0200

    handle the 2D array and test

--- Machine.java
+++ Machine.java
@@ -1,16 +1,41 @@
 public class Machine {
-    
-    
-    
+
+    private Item[][] items;
+
+    //It uses a nested loop to copy every object from the parameter into the field.
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
+
     /**
      * Function name – dispense
-     * @param row (int)
+     * 
+     * @param row  (int)
      * @param spot (int)
      * @return (boolean)
      * 
-     * Inside the function:
-     *  1. Checks if the requested item has a quantity bigger than 0.
-     *      • if so: decreases its quantity by one and returns true.
-     *      • otherwise: returns false.
+     *         Inside the function:
+     *         1. Checks if the requested item has a quantity bigger than 0.
+     *         • if so: decreases its quantity by one and returns true.
+     *         • otherwise: returns false.
      */
+
+    public boolean dispense(int row, int spot) {
+
+        return false;
+    }
+
 }
